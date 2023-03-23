e102ba2ef9a40a46900e5a06f8dca15e858a453b
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-03-23 22:05:24 +0700

    Update: add item listholder for savedlist class

--- app/src/main/java/com/example/stayfinder/SavedList.kt
+++ app/src/main/java/com/example/stayfinder/SavedList.kt
@@ -2,6 +2,7 @@ package com.example.stayfinder
 class SavedList:java.io.Serializable{
     public var titlename: String? = ""
     public var props: String? = ""
+    var items : List<SavedListItem>? = null
 
     constructor(stitlename: String, sname: String){
         titlename = stitlename
