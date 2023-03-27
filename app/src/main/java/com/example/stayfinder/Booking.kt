d9448c0a0ed35a412933b863beb03b907226d054
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-03-27 18:47:33 +0700

    update(ui): booking_change_setting

--- app/src/main/java/com/example/stayfinder/Booking.kt
+++ app/src/main/java/com/example/stayfinder/Booking.kt
@@ -3,48 +3,22 @@ package com.example.stayfinder
 import android.os.Parcel
 import android.os.Parcelable
 
-class Booking(titlename: String?, period: String?, price: Double,status: String?, img: Int?):Parcelable {
-    public var titlename: String? = ""
-    public var period: String? = ""
-    public var price: Double? = 0.0
-    public var status: String? = ""
-    public var img: Int? = null
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readDouble(),
-        parcel.readString(),
-        parcel.readInt()
-    ) {
-        titlename = parcel.readString()
-        period = parcel.readString()
-        price = parcel.readDouble()
-        status = parcel.readString()
-        img = parcel.readInt()
-    }
-    init {
+class Booking() {
+    var titlename: String? = ""
+    var period: String? = ""
+    var price: Double? = 0.0
+    var status: String? = ""
+    var img: Int? = null
+
+    constructor(titlename: String, period: String, price: Double,status: String, img: Int) : this()
+    {
         this.titlename = titlename
         this.period = period
-        this.price = price
-        this.status = status
-        this.img = img
+        this.price= price
+        this.status= status
+        this.img=img
     }
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
+    constructor(titlename: String):this(){
+        this.titlename = titlename
     }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Booking> {
-        override fun createFromParcel(parcel: Parcel): Booking {
-            return Booking(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Booking?> {
-            return arrayOfNulls(size)
-        }
-    }
-
 }
