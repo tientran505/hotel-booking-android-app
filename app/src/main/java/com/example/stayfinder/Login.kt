f3c2492fa222cba92f5df54160b605ee1cc30df6
Tien <73010955+tientran505@users.noreply.github.com>
2023-03-30 08:58:41 +0700

    refactor: merge google signin feat

--- app/src/main/java/com/example/stayfinder/Login.kt
+++ app/src/main/java/com/example/stayfinder/Login.kt
@@ -7,10 +7,21 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.delay
@@ -19,8 +30,15 @@ class Login : AppCompatActivity() {
     private var emailET: EditText? = null
     private var pwET: EditText? = null
     private var signInBtn: AppCompatButton? = null
+    private var ggLoginBtn: MaterialButton? = null
 
     private var progressDialog: ProgressDialog? = null
+
+    private val RC_SIGN_IN = 9723;
+    private lateinit var mGoogleSignInClient: GoogleSignInClient;
+    private lateinit var mGoogleSignInOptions: GoogleSignInOptions;
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +51,13 @@ class Login : AppCompatActivity() {
         menu?.setHomeButtonEnabled(true)
 
         progressDialog = ProgressDialog(this)
+        auth = Firebase.auth
+
+        createRequest()
 
         signInBtn?.setOnClickListener {
             val email = emailET?.text.toString()
             val password = pwET?.text.toString()
-
-            val auth: FirebaseAuth = Firebase.auth
 
             progressDialog?.setTitle("Please wait")
             progressDialog?.setMessage("Verifying...")
@@ -67,6 +86,11 @@ class Login : AppCompatActivity() {
                     progressDialog?.dismiss()
                 }
         }
+
+        ggLoginBtn = findViewById(R.id.logInWithGGBtn)
+        ggLoginBtn?.setOnClickListener {
+            goToSignIn()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -83,5 +107,70 @@ class Login : AppCompatActivity() {
         emailET = findViewById(R.id.inputUsernameEt)
         pwET = findViewById(R.id.inputPasswordEt)
         signInBtn = findViewById(R.id.signInBtn)
+    }
+
+    private fun createRequest(){
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+    }
+
+    private fun goToSignIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn
+                .getSignedInAccountFromIntent(data)
+
+
+            try {
+                val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+                val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                progressDialog?.setTitle("Login with gmail")
+                progressDialog?.setMessage("Verifying...")
+                progressDialog?.show()
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this) { it ->
+                        if (it.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            Log.i("userLog", user.toString())
+
+                            Toast.makeText(this, "Login with email successful.",
+                                Toast.LENGTH_SHORT).show()
+
+                            Handler().postDelayed(Runnable {
+                                val intent = Intent(this, MainActivity::class.java)
+                                intent.putExtra("fragment", "profile")
+                                startActivity(intent)
+                                finishAffinity()
+                            }, 2000)
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                        progressDialog?.dismiss()
+                    }
+
+                //reload activity
+                this.finish()
+                this.startActivity(this.intent)
+
+            } catch (e: ApiException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                Log.e("BUG===========", e.message!!)
+            }
+        }
     }
 }
