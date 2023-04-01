f14e6c7991bbfcdbedd1efa268e87a0ba11426ca
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-04-02 02:59:25 +0700

    build: activity_google_map search by address

--- app/src/main/java/com/example/stayfinder/SubBookingDetailAddress.kt
+++ app/src/main/java/com/example/stayfinder/SubBookingDetailAddress.kt
@@ -1,12 +1,32 @@
 package com.example.stayfinder
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,51 +38,104 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SubBookingDetailAddress.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SubBookingDetailAddress : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class SubBookingDetailAddress : Fragment(), OnMapReadyCallback{
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedClient: FusedLocationProviderClient;
+    private lateinit var currentLocation : Location
+    private var REQUEST_CODE = 101;
+//    lateinit var addressET: EditText
+//    lateinit var searchBT: Button
+    val address = "135 Trần Hưng Đạo, phường Cầu Ông Lãnh, Quận 1, Thành phố Hồ Chí Minh"
+    var addressfind =""
+    lateinit var layout: RelativeLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_sub_booking_detail_address, container, false)
-        val button : Button? = view.findViewById(R.id.button)
-        button!!.setOnClickListener{
-            val intent = Intent(this.context,MapsActivity::class.java)
-            startActivity(intent)
-        }
+        searchMap()
+
 
         return view
     }
+    fun searchMap(){
+        if(ActivityCompat.checkSelfPermission(this.requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this.requireContext(),android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this.requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE)
+            return
+        }
+        val templatlng = getLocationFromAddress(address)
+        if(templatlng != null){
+            currentLocation = Location(LocationManager.NETWORK_PROVIDER)
+            currentLocation.latitude = templatlng.latitude
+            currentLocation.longitude = templatlng.longitude
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubBookingDetailCostAddress.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubBookingDetailAddress().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            val geocoder: Geocoder
+            val addressestemp: List<Address>?
+            geocoder = Geocoder(this.requireContext(), Locale.getDefault())
+
+            addressestemp = geocoder.getFromLocation(
+                templatlng.latitude,
+                templatlng.longitude,
+                1
+            ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            addressfind=""
+            addressfind += addressestemp!![0].getAddressLine(0)// If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            addressfind+= addressestemp!![0].locality
+            addressfind+= addressestemp!![0].adminArea
+            addressfind+= addressestemp!![0].countryName
+            addressfind+= addressestemp!![0].postalCode
+            addressfind+= addressestemp!![0].featureName
+
+            Toast.makeText(context ,addressfind, Toast.LENGTH_LONG).show()
+            val mapFragment = getParentFragmentManager()
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
+
+
     }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(REQUEST_CODE){
+            REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+//                getLocationUser()
+                searchMap()
+            }
+        }
+    }
+    fun getLocationFromAddress( strAddress: String?): LatLng? {
+        val coder = Geocoder(this.requireContext(), Locale.getDefault())
+        val address: List<Address>?
+        var LatLan: LatLng? = null
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress!!, 5)
+            if (address == null) {
+                return null
+            }
+            val location = address[0]
+            LatLan = LatLng(location.latitude, location.longitude)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+        return LatLan
+    }
+    override fun onMapReady(googleMap: GoogleMap) {//        mMap = googleMap
+
+        val latLng = LatLng(currentLocation.latitude,currentLocation.longitude)
+        val marketOptions = MarkerOptions().position(latLng).title("Current Location")
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17f))
+        googleMap?.addMarker(marketOptions)
+    }
+
+
 }
