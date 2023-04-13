f46f46a309b0966a51299f9b4d444c24ccf17c34
Le Van Dong <itlvd@outlook.com>
2023-04-13 22:30:09 +0700

    build: Add new xml add hotel

---
+++ app/src/main/java/com/example/stayfinder/AddHotelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.stayfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_hotel)
+    }
+}
