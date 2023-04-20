60f91a8f1e904d9538939f6d324f417fb3d62334
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-04-20 09:33:49 +0700

    resolve conflict

--- app/src/main/java/com/example/stayfinder/HotelDetailActivity.kt
+++ app/src/main/java/com/example/stayfinder/HotelDetailActivity.kt
@@ -2,7 +2,6 @@ package com.example.stayfinder
 
 import android.app.ProgressDialog
 import android.content.Intent
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
@@ -18,7 +17,7 @@ import java.io.Serializable
 import com.example.stayfinder.booking.PersonalConfirmation
 import java.net.URL
 data class hotelss(
-    var hotel_id: String = "",
+    var id: String = "",
     var hotel_name: String = "",
     var description: String = "",
     var address: address = address(),
@@ -34,7 +33,7 @@ data class hotelss(
 //        0, 0)
 }
 data class HotelDetails(
-    var id: String ="",
+    var id: String = "",
     var hotel_name: String ="",
     var priceless: Double =0.0,
     var img: ArrayList<URL> =ArrayList<URL>(),
@@ -46,8 +45,8 @@ data class HotelDetails(
     ):Serializable{
     constructor(id: String,hotel_name: String,pricebernight: Double, address: address,img: ArrayList<URL>,rating_overall: Double,description: String):
             this(id,hotel_name,pricebernight,img,rating_overall,address,description,0,0)
-    constructor(a: hotelss):this(a.hotel_id,a.hotel_name,0.0, convertStringtoURL(a.photoUrl),a.rating_overall,a.address,a.description,a.comment_count,a.booking_count)
-    constructor(a: hotelss, priceless: Double):this(a.hotel_id,a.hotel_name,priceless, convertStringtoURL(a.photoUrl),a.rating_overall,a.address,a.description,a.comment_count,a.booking_count)
+    constructor(a: hotelss):this(a.id,a.hotel_name,0.0, convertStringtoURL(a.photoUrl),a.rating_overall,a.address,a.description,a.comment_count,a.booking_count)
+    constructor(a: hotelss, priceless: Double):this(a.id,a.hotel_name,priceless, convertStringtoURL(a.photoUrl),a.rating_overall,a.address,a.description,a.comment_count,a.booking_count)
 
 }
 fun convertStringtoURL(a: ArrayList<String>):ArrayList<URL>{
@@ -60,50 +59,52 @@ fun convertStringtoURL(a: ArrayList<String>):ArrayList<URL>{
 class HotelDetailActivity : AppCompatActivity() , CoroutineScope by MainScope() {
     var hoteldetails:HotelDetails? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-    
+
         super.onCreate(savedInstanceState)
-                initActionBar()
+        initActionBar()
         val documents = Firebase.firestore.collection("Hotels")
             .document("5l5PibkyeRaZRFCVPrlB")
         documents.get().addOnSuccessListener { document ->
-                if (document != null) {
-                    val l = document.toObject(hotelss::class.java)
-                    hoteldetails = l?.let { HotelDetails(it) }
-                    setContentView(R.layout.activity_hotel_detail)
-                    println(hoteldetails)
-                    val fm: FragmentManager = supportFragmentManager
-                    val dateStart ="30-3-2023"
-                    val dateEnd = "1-4-2023"
-                    val detailRoom= arrayListOf<Int>(1,2,0)
-                    val bundle = Bundle()
-                    bundle.putSerializable("BookingDetail", hoteldetails)
-                    bundle.putString("dateStart", dateStart)
-                    bundle.putString("dateEnd", dateEnd)
-                    bundle.putIntegerArrayList("detailRoom",detailRoom)
-                    val fragImage = SubHotelDetailImage()
-                    fragImage.setArguments(bundle);
-                    val fragPeriod = SubHotelDetailPeriod()
-                    fragPeriod.setArguments(bundle);
-                    val fragAddress = SubHotelDetailAddress()
-                    fragAddress.setArguments(bundle);
-                    val fragDescription = SubHotelDetailDescription()
-                    fragDescription.setArguments(bundle);
-                    fm.beginTransaction().replace(R.id.fame1, fragImage).commit();
-                    fm.beginTransaction().replace(R.id.fame2, fragPeriod).commit();
-                    fm.beginTransaction().replace(R.id.fame3, fragAddress).commit();
-                    fm.beginTransaction().replace(R.id.fame4, fragDescription).commit();
-                    val bookingBtn = findViewById<Button>(R.id.BookingBtn)
-                    bookingBtn.setOnClickListener(){
-                        val intent = Intent(this, RoomActivity::class.java)
-                        intent.putExtra("hotel_id",hoteldetails?.id);
-                        startActivity(intent)
-                    }
-                } else {
-
+            if (document != null) {
+                val l = document.toObject(hotelss::class.java)
+                println(l)
+                hoteldetails = l?.let { HotelDetails(it) }
+                setContentView(R.layout.activity_hotel_detail)
+                println(hoteldetails)
+                val fm: FragmentManager = supportFragmentManager
+                val dateStart = "30-3-2023"
+                val dateEnd = "1-4-2023"
+                val detailRoom = arrayListOf<Int>(1, 2, 0)
+                val bundle = Bundle()
+                bundle.putSerializable("BookingDetail", hoteldetails)
+                bundle.putString("dateStart", dateStart)
+                bundle.putString("dateEnd", dateEnd)
+                bundle.putIntegerArrayList("detailRoom", detailRoom)
+                val fragImage = SubHotelDetailImage()
+                fragImage.setArguments(bundle);
+                val fragPeriod = SubHotelDetailPeriod()
+                fragPeriod.setArguments(bundle);
+                val fragAddress = SubHotelDetailAddress()
+                fragAddress.setArguments(bundle);
+                val fragDescription = SubHotelDetailDescription()
+                fragDescription.setArguments(bundle);
+                fm.beginTransaction().replace(R.id.fame1, fragImage).commit();
+                fm.beginTransaction().replace(R.id.fame2, fragPeriod).commit();
+                fm.beginTransaction().replace(R.id.fame3, fragAddress).commit();
+                fm.beginTransaction().replace(R.id.fame4, fragDescription).commit();
+                val bookingBtn = findViewById<Button>(R.id.BookingBtn)
+                bookingBtn.setOnClickListener() {
+                    val intent = Intent(this, RoomActivity::class.java)
+                    intent.putExtra("hotel_id", hoteldetails?.id);
+                    startActivity(intent)
                 }
+            } else {
+
             }
+        }
             .addOnFailureListener { exception ->
             }
+    }
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
