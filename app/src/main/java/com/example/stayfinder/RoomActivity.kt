a5394b24ad91eeb665d6daf266d9efa0809f85fb
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-04-29 02:04:36 +0700

    update room data

--- app/src/main/java/com/example/stayfinder/RoomActivity.kt
+++ app/src/main/java/com/example/stayfinder/RoomActivity.kt
@@ -13,10 +13,10 @@ class RoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
         val Roomlist = arrayListOf<Room>(
-//            Room(123,345,room_type.double,"2 3 con mực, anh iu em cực",
-//            arrayListOf(URL("https://i.pinimg.com/736x/50/d8/62/50d862e0e5d9b35882749cc1b53a03ef.jpg"),URL("https://i.pinimg.com/564x/5f/8d/6a/5f8d6a6a489b521364504cac30803d49.jpg")),"12-12-1001",45.2,20.3,0.5,"Giá cho 2 đêm từ ngày ... đến ngày ...",2)
-//            ,      Room(123,345,room_type.double,"2 3 con mực, anh iu em cực",
-//            arrayListOf(URL("https://i.pinimg.com/736x/50/d8/62/50d862e0e5d9b35882749cc1b53a03ef.jpg"),URL("https://i.pinimg.com/564x/5f/8d/6a/5f8d6a6a489b521364504cac30803d49.jpg")),"12-12-1001",45.2,20.3,0.5,"Giá cho 2 đêm từ ngày ... đến ngày ...",2)
+            Room(123,345,room_type.double,"2 3 con mực, anh iu em cực",
+            arrayListOf(("https://i.pinimg.com/736x/50/d8/62/50d862e0e5d9b35882749cc1b53a03ef.jpg"),("https://i.pinimg.com/564x/5f/8d/6a/5f8d6a6a489b521364504cac30803d49.jpg")),"12-12-1001",45.2,20.3,0.5,"Giá cho 2 đêm từ ngày ... đến ngày ...",2)
+            ,      Room(123,345,room_type.double,"2 3 con mực, anh iu em cực",
+            arrayListOf(("https://i.pinimg.com/736x/50/d8/62/50d862e0e5d9b35882749cc1b53a03ef.jpg"),("https://i.pinimg.com/564x/5f/8d/6a/5f8d6a6a489b521364504cac30803d49.jpg")),"12-12-1001",45.2,20.3,0.5,"Giá cho 2 đêm từ ngày ... đến ngày ...",2)
 
         )
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
