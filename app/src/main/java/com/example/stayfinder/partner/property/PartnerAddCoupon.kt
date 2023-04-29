273d709d5682349b3cfd0425d0867bdbcb231976
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-04-30 01:03:26 +0700

    add: basic UI for add coupon

---
+++ app/src/main/java/com/example/stayfinder/partner/property/PartnerAddCoupon.kt
@@ -0,0 +1,11 @@
+package com.example.stayfinder.partner.property
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PartnerAddCoupon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_partner_add_coupon)
+    }
+}
