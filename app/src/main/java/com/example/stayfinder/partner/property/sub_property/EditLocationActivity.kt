1682baacdbd1169eb441488a014a9f2f5a621ab2
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-05-05 23:21:26 +0700

    save update address

--- app/src/main/java/com/example/stayfinder/partner/property/sub_property/EditLocationActivity.kt
+++ app/src/main/java/com/example/stayfinder/partner/property/sub_property/EditLocationActivity.kt
@@ -5,47 +5,124 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.stayfinder.R
+import com.example.stayfinder.address
 import com.example.stayfinder.databinding.ActivityEditLocationBinding
+import com.example.stayfinder.hotels
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import java.io.IOException
 import java.util.*
 
-class EditLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
+class EditLocationActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var progressBar: ProgressBar
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityEditLocationBinding
     private lateinit var fusedClient: FusedLocationProviderClient;
     private lateinit var currentLocation : Location
     private var REQUEST_CODE = 101;
-    var address = ""
-    var addressfind =""
+    var address: String =""
+    lateinit var addressFb :address
+    lateinit var currentLocationBtn: Button
+    lateinit var searchET: EditText
+    lateinit var searchBtn: Button
+    lateinit var saveBtn: Button
+    private lateinit var defaultLocation: Location
+    var lastKnownLocation: Location? = null
+    private var locationPermissionGranted: Boolean= true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityEditLocationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        address = "315A Trần Hưng Đạo phường Cầu Ông Lãnh Quận 1"
-        //        fusedClient = LocationServices.getFusedLocationProviderClient(this)
+        currentLocationBtn = findViewById(R.id.currentLocationBtn)
+        searchET = findViewById(R.id.searchET)
+        searchBtn = findViewById(R.id.searchBtn)
+        saveBtn = findViewById(R.id.saveBtn)
+        progressBar = findViewById(R.id.savedListPB)
+        progressBar.visibility = View.VISIBLE
+
+        val bundle = intent.extras
+//        val hotel_id = bundle!!.getString("hotel_id")!!
+        val hotel_id="5l5PibkyeRaZRFCVPrlB"
+        val db = Firebase.firestore;
+        val documents = Firebase.firestore.collection("Hotels")
+            .document(hotel_id)
+            .get()
+            .addOnSuccessListener  { document ->
+            if (document != null) {
+                val hotels = document.toObject(hotels::class.java)!!
+                val addressTemp = hotels?.address as address
+                address = addressTemp.number+" "+ addressTemp.street+", "+ addressTemp.district+", "+ addressTemp.ward+", "+addressTemp.city
+                searchMap(address)
+                progressBar.visibility = View.GONE
+            }
+            else{
+                address = "Việt Nam"
+                searchMap(address)
+                progressBar.visibility = View.GONE
+
+            }
+        }
+        fusedClient = LocationServices.getFusedLocationProviderClient(this)
+        searchBtn.setOnClickListener {
+            if(searchET.text.toString()!= ""&&searchET.text.toString()!= null){
+                address = searchET.text.toString()
+                searchMap(address)
+            }
+        }
+        currentLocationBtn.setOnClickListener {
+            getLocationUser()
+        }
+        saveBtn.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            println(address)
+            println(addressFb)
+            val docRef = db.collection("Hotels").document(hotel_id)
+            docRef.update("address", addressFb)
+                .addOnSuccessListener {
+                    // Update successful
+                    progressBar.visibility = View.GONE
+
+                }
+                .addOnFailureListener { e ->
+                    // Handle errors
+                    progressBar.visibility = View.GONE
+
+                }
+
+        }
 //        searchMap()
-        fusedClient= LocationServices.getFusedLocationProviderClient(this);
-        getLocationUser()
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        val mapFragment = supportFragmentManager
-//            .findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
+//        getLocationUser()
+    }
+
+    private fun convertLocationtoAddress(currentLocation: Location) {
+        val geocoder: Geocoder =Geocoder(this, Locale.getDefault())
+        val addressestemp: List<Address>?
+        addressestemp = geocoder.getFromLocation(
+            currentLocation.latitude,
+            currentLocation.longitude,
+            1
+        ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        address = addressestemp!![0].getAddressLine(0)// If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        addressFb = address(addressestemp[0] )
     }
 
     private fun getLocationUser(){
@@ -56,25 +133,19 @@ class EditLocationActivity : AppCompatActivity(), OnMapReadyCallback {
                 REQUEST_CODE)
             return
         }
-        println("hello location")
         val getLocation = fusedClient.lastLocation.addOnSuccessListener {
                 location ->
-            println("clocation" + location)
             if(location!=null){
                 currentLocation = location
-                Toast.makeText(applicationContext,currentLocation.latitude.toString()+ " "+ currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
+                convertLocationtoAddress(currentLocation)
                 val mapFragment = supportFragmentManager
                     .findFragmentById(R.id.map) as SupportMapFragment
                 mapFragment.getMapAsync(this)
-                print("location"+currentLocation.toString())
-            }
-            else{
-                println("cannot get current location")
             }
         }
 
     }
-    fun searchMap(){
+    fun searchMap(address: String){
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
@@ -83,38 +154,16 @@ class EditLocationActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
 
-        val templatlng = getLocationFromAddress(address)
-
-        if(templatlng != null){
+        val point = getLocationFromAddress(address)
+        if(point != null){
             currentLocation = Location(LocationManager.NETWORK_PROVIDER)
-            currentLocation.latitude = templatlng.latitude
-            currentLocation.longitude = templatlng.longitude
-
-
-            val geocoder: Geocoder
-            val addressestemp: List<Address>?
-            geocoder = Geocoder(this, Locale.getDefault())
-
-            addressestemp = geocoder.getFromLocation(
-                templatlng.latitude,
-                templatlng.longitude,
-                1
-            ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-            addressfind=""
-            addressfind += addressestemp!![0].getAddressLine(0)// If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-            addressfind+= addressestemp!![0].locality
-            addressfind+= addressestemp!![0].adminArea
-            addressfind+= addressestemp!![0].countryName
-            addressfind+= addressestemp!![0].postalCode
-            addressfind+= addressestemp!![0].featureName
-
+            currentLocation.latitude = point.latitude
+            currentLocation.longitude = point.longitude
+            convertLocationtoAddress(currentLocation)
             val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
             mapFragment.getMapAsync(this)
-
         }
-
-
     }
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -124,38 +173,59 @@ class EditLocationActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when(REQUEST_CODE){
             REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                getLocationUser()
-//                searchMap()
             }
         }
     }
     fun getLocationFromAddress( strAddress: String?): LatLng? {
-
         val coder = Geocoder(this, Locale.getDefault())
         val addressList: List<Address>?
         var LatLan: LatLng? = null
-
         try {
             addressList = coder.getFromLocationName(strAddress!!, 1)
-
             if (addressList == null) {
                 return null
             }
             val location = addressList[0]
+            println(location)
+            addressFb = address(location)
             LatLan = LatLng(location.latitude, location.longitude)
         } catch (ex: IOException) {
             ex.printStackTrace()
         }
-
         return LatLan
     }
-    override fun onMapReady(googleMap: GoogleMap) {
-
-        val latLng = LatLng(currentLocation.latitude,currentLocation.longitude)
-        val marketOptions = MarkerOptions().position(latLng).title(addressfind+ "CurrentLocation")
-
-        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17f))
-        googleMap?.addMarker(marketOptions)
+    override fun onMapReady(mMap: GoogleMap) {
+        var latLng = LatLng(currentLocation.latitude,currentLocation.longitude)
+        mMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17f))
+        var marker: Marker? = null
+        marker?.remove()
+        marker = mMap?.addMarker(MarkerOptions().position(latLng).title(address))
+        mMap.setOnMapClickListener { point -> //save current location
+            latLng = point
+            var addresses: ArrayList<Address> = ArrayList()
+            try {
+                val geocoder: Geocoder =Geocoder(this, Locale.getDefault())
+                addresses = geocoder.getFromLocation( point.latitude, point.longitude, 1) as ArrayList<Address>
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            val addressTemp = addresses[0]
+            println("Address[0]"+ addressFb)
+            if (addressTemp != null) {
+                address = addressTemp.getAddressLine(0)
+                addressFb = address(addressTemp)
+                currentLocation = Location(LocationManager.NETWORK_PROVIDER)
+                currentLocation.latitude = point.latitude
+                currentLocation.longitude = point.longitude
+            }
+            //remove previously placed Marker
+            //place marker where user just clicked
+            marker?.remove()
+            marker = mMap.addMarker(
+                MarkerOptions().position(point).title(address)
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+            )
+        }
     }
 }
