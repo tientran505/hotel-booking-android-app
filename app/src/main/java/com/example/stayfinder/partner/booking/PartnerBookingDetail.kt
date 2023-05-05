5bc432d06f16b3bae8608412b7287a192712246a
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-05-06 00:39:11 +0700

    add: create activity for booking detail

---
+++ app/src/main/java/com/example/stayfinder/partner/booking/PartnerBookingDetail.kt
@@ -0,0 +1,12 @@
+package com.example.stayfinder.partner.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stayfinder.R
+
+class PartnerBookingDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.partner_activity_booking_detail)
+    }
+}
