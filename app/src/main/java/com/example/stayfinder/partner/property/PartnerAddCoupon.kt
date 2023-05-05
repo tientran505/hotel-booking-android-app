356f7ec577e11c0fd9d0ff22404c1db5ec617caa
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-05-05 23:34:19 +0700

    update: add coupon UI

--- app/src/main/java/com/example/stayfinder/partner/property/PartnerAddCoupon.kt
+++ app/src/main/java/com/example/stayfinder/partner/property/PartnerAddCoupon.kt
@@ -2,10 +2,11 @@ package com.example.stayfinder.partner.property
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stayfinder.R
 
 class PartnerAddCoupon : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_partner_add_coupon)
+        setContentView(R.layout.partner_activity_add_coupon)
     }
 }
