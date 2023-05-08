8171550f94587f29762bc3a082119c1f3ba92e37
Le Van Dong <itlvd@outlook.com>
2023-05-08 11:50:49 +0700

    build: edit room at screen 1

--- app/src/main/java/com/example/stayfinder/services/room/RoomAddHotelDetailActivity.kt
+++ app/src/main/java/com/example/stayfinder/services/room/RoomAddHotelDetailActivity.kt
@@ -6,22 +6,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.stayfinder.R
-import com.example.stayfinder.model.HotelDetailModel
 import com.example.stayfinder.model.RoomDetailModel
-import com.example.stayfinder.services.hotel.MapAddHotelActivity
-import com.google.firebase.Timestamp
+import com.example.stayfinder.partner.PartnerMainActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.mcdev.quantitizerlibrary.HorizontalQuantitizer
-
 import com.nex3z.togglebuttongroup.MultiSelectToggleGroup
 import com.nex3z.togglebuttongroup.button.LabelToggle
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class RoomAddHotelDetailActivity : AppCompatActivity() {
-    val arr_typeroom = arrayListOf<String>("Bed Room","Bath Room","Kitchen Room")
+    val arr_typeroom = arrayListOf<String>("Bed Room", "Bath Room", "Kitchen Room")
     var typesRoom: ArrayList<String> = ArrayList<String>()
     lateinit var guestStayHQ: HorizontalQuantitizer
     lateinit var bathroomHQ: HorizontalQuantitizer
@@ -29,9 +29,17 @@ class RoomAddHotelDetailActivity : AppCompatActivity() {
     lateinit var areaSquareET: EditText
     lateinit var nextBtn: Button
     lateinit var categoryRoomSpinner: Spinner
+
+    var editMode = false
+    private var db: FirebaseFirestore? = null
+    var nameCollection: String = "TestRoom"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_add_hotel_detail)
+
+        db = Firebase.firestore
 
         categoryRoomSpinner = findViewById(R.id.spinnerTypeRoom)
         guestStayHQ = findViewById(R.id.guestStay_HQ)
@@ -40,18 +48,72 @@ class RoomAddHotelDetailActivity : AppCompatActivity() {
         areaSquareET = findViewById(R.id.areaET)
         nextBtn = findViewById(R.id.nextBtn)
         var descriptionEt = findViewById<EditText>(R.id.descriptionEt)
+        val multi = findViewById<View>(R.id.roomSelected_MultiGroup) as MultiSelectToggleGroup
 
-        //var uuidHotel: String? = intent.extras?.getString("uuidHotel")
-        var uuidHotel = "ddddddddd"
-        var uuidRoom:String = UUID.randomUUID().toString()
+        var uuidRoom: String? = intent.extras?.getString("uuidRoom")
+        var uuidHotel: String? = intent.extras?.getString("uuidHotel")
+
+        uuidHotel = "ddddddddd"
+        uuidRoom = "6a02338a-f780-484b-891e-b0e3ccdb116e"
+
+        if (uuidHotel != null && uuidRoom == null) {
+            uuidRoom = UUID.randomUUID().toString()
+            editMode = false
+        } else if (uuidHotel != null && uuidRoom != null) { // entry exist - > fill form
+            editMode = true
+
+
+            val docRef = db!!.collection(nameCollection!!).document(uuidRoom)
+            docRef.get().addOnSuccessListener { document ->
+                if (document != null) {
+                    // fill out the form
+                    val room = document.toObject(RoomDetailModel::class.java)!!
+
+                    // set spinner style room
+                    var typeRoom = applicationContext.resources.getStringArray(R.array.spinner_type_room)
+                    val indexTypeRoom = typeRoom.indexOf(room.room_type)
+                    categoryRoomSpinner.setSelection(indexTypeRoom)
+
+                    //set has room
+                    val hasRoom = room.have_room
+                    for(i in 0 until multi.childCount){
+                        var toggle = multi.getChildAt(i) as LabelToggle
+                        if(hasRoom.contains(toggle.text)){
+                            toggle.isChecked = true
+                        }
+                    }
+
+                    guestStayHQ.value = room.num_guest!!
+                    bathroomHQ.value = room.num_bathroom!!
+                    bedroomHQ.value = room.num_bedroom!!
+                    areaSquareET.setText(room.areaSquare!!.toString())
+                    descriptionEt.setText(room.description.toString())
+
+                } else {
+                    Toast.makeText(this, "No document to show", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener { ex ->
+                Toast.makeText(this, "Fail to get an entry. Ex: $ex", Toast.LENGTH_SHORT).show()
+            }
+
+
+        } else {
+            editMode = false
+            Toast.makeText(this, "Error loading room", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, PartnerMainActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }
+
+        //var uuidRoom:String = UUID.randomUUID().toString()
 //        var hotel = intent.getSerializableExtra("hotelInfo") as HotelDetailModel?
 //        var tempUriImage = intent.getStringArrayListExtra("uriImage")
         //Toast.makeText(this, txt.toString(),Toast.LENGTH_SHORT).show()
 
-        val multi = findViewById<View>(R.id.roomSelected_MultiGroup) as MultiSelectToggleGroup
+
         multi.setOnCheckedChangeListener { group, checkedId, isChecked ->
             typesRoom = ArrayList<String>()
-            for(checkedId in group.checkedIds){
+            for (checkedId in group.checkedIds) {
                 var toggle = multi.findViewById<LabelToggle>(checkedId)
                 typesRoom.add(toggle.text as String)
             }
@@ -75,7 +137,9 @@ class RoomAddHotelDetailActivity : AppCompatActivity() {
                 num_guest = guestStayHQ.value,
                 num_bedroom = bedroomHQ.value,
                 num_bathroom = bathroomHQ.value,
-                areaSquare = if(areaSquareET.text.toString().isNotEmpty()) areaSquareET.text.toString().toDouble() else null as Double?
+                areaSquare = if (areaSquareET.text.toString()
+                        .isNotEmpty()
+                ) areaSquareET.text.toString().toDouble() else null as Double?
             )
 
             var intent = Intent(this, RoomAddHotelDetailStep2Activity::class.java)
@@ -83,11 +147,6 @@ class RoomAddHotelDetailActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-
-
-
-
-
 
 
     }
