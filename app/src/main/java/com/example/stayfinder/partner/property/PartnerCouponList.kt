f30b6cb53396ebaff3317945741a7c6f56fce33f
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-05-10 00:58:50 +0700

    update: navigation

--- app/src/main/java/com/example/stayfinder/partner/property/PartnerCouponList.kt
+++ app/src/main/java/com/example/stayfinder/partner/property/PartnerCouponList.kt
@@ -1,17 +1,74 @@
 package com.example.stayfinder.partner.property
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.stayfinder.R
+import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stayfinder.*
+import com.example.stayfinder.partner.property.adapter.CouponInfoAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class PartnerCouponList : AppCompatActivity() {
 
+    var couponList = arrayListOf<coupon>()
+    val db = Firebase.firestore
+    val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+    lateinit var listadapter:CouponInfoAdapter
+    private suspend fun loadSavedLists() {
+        val documents = Firebase.firestore.collection("coupons")
+            .whereEqualTo("owner_id", "blabla")
+            .get()
+            .await()
+        for (document in documents) {
+            val l = document.toObject(coupon::class.java)
+            couponList.add(l)
+            listadapter.notifyItemInserted(couponList.size - 1)
+        }
 
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.partner_activity_coupon_list)
 
+        CoroutineScope(MainScope().coroutineContext).launch {
+            loadSavedLists()
+        }
 
+        val cpList = findViewById<RecyclerView>(R.id.recyclerView) as RecyclerView
+        listadapter = CouponInfoAdapter(couponList)
+        cpList.adapter = listadapter
+        cpList.layoutManager = LinearLayoutManager(this)
+
+        listadapter.setOnIconClickListener((object:CouponInfoAdapter.onIconClickListener{
+            override fun onIconClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+        }))
+
+        listadapter.setOnItemClickListener((object : CouponInfoAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+        }))
+
+        var addbtn = findViewById<Button>(R.id.button)
+        addbtn.setOnClickListener{
+            val intent = Intent(this, PartnerAddCoupon::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/stayfinder/partner/property/adapter/CouponInfoAdapter.kt
+++ app/src/main/java/com/example/stayfinder/partner/property/adapter/CouponInfoAdapter.kt
@@ -1,15 +1,83 @@
 package com.example.stayfinder.partner.property.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.stayfinder.SavedListAdapter
+import com.example.stayfinder.R
 import com.example.stayfinder.coupon
 
 class CouponInfoAdapter(private val lists: List<coupon>):
     RecyclerView.Adapter<CouponInfoAdapter.ViewHolder>() {
 
-    inner class ViewHolder(listItemView: View, mlistener: CouponInfoAdapter.onItemClickListener, ilistener: CouponInfoAdapter.onIconClickListener)
-        : RecyclerView.ViewHolder(listItemView) {
+    inner class ViewHolder(listItemView: View, mlistener: CouponInfoAdapter.onItemClickListener,
+                           ilistener: CouponInfoAdapter.onIconClickListener) : RecyclerView.ViewHolder(listItemView) {
+        val nameTv = listItemView.findViewById<TextView>(R.id.promotionName)
+        val discountValueTv = listItemView.findViewById<TextView>(R.id.discountValue)
+        val startDateTv = listItemView.findViewById<TextView>(R.id.startDate)
+        val endDateTv = listItemView.findViewById<TextView>(R.id.endDate)
+        val iconTv = listItemView.findViewById<ImageButton>(R.id.imageButton)
+
+        init{
+            listItemView.setOnClickListener{
+                mlistener.onItemClick(adapterPosition)
+            }
+
+            iconTv.setOnClickListener({
+                ilistener.onIconClick(adapterPosition)
+            })
 
         }
+        }
+
+    private lateinit var mListener : CouponInfoAdapter.onItemClickListener
+    private lateinit var iListener : CouponInfoAdapter.onIconClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    interface onIconClickListener {
+
+        fun onIconClick(position: Int)
+    }
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
+    }
+
+    fun setOnIconClickListener(listener: onIconClickListener){
+        iListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+
+            ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+// Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.partner_custom_coupon, parent, false)
+// Return a new holder instance
+        return ViewHolder(contactView, mListener, iListener)
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+
+    override fun onBindViewHolder(holder: CouponInfoAdapter.ViewHolder, position: Int) {
+
+        val nameView = holder.nameTv
+        val discountValue = holder.discountValueTv
+        val startDate = holder.startDateTv
+        val endDate = holder.endDateTv
+
+        val discount = this.lists[position].discount.toDouble() * 100
+
+        nameView.setText(this.lists[position].title)
+        discountValue.setText(discount.toString() + "%")
+        startDate.setText(this.lists[position].startDate)
+        endDate.setText(this.lists[position].endDate)
+    }
 }
