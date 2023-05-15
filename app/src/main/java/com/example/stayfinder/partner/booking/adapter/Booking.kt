aeba60f2d116e3063fa8af2ca4bad8893b41879a
Tien <73010955+tientran505@users.noreply.github.com>
2023-05-15 20:39:30 +0700

    feat: show booking list

--- app/src/main/java/com/example/stayfinder/partner/booking/adapter/Booking.kt
+++ app/src/main/java/com/example/stayfinder/partner/booking/adapter/Booking.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.stayfinder.BookingDetail
 import com.example.stayfinder.R
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 data class Booking(
     val title: String,
@@ -19,7 +24,7 @@ data class Booking(
     val totalPrice: String,
 )
 
-class PartnerBookingAdapter(private val myList: ArrayList<Booking>) :
+class PartnerBookingAdapter(private val myList: ArrayList<BookingDetail>) :
     RecyclerView.Adapter<PartnerBookingAdapter.ViewHolder>() {
 
     inner class ViewHolder(listItem: View): RecyclerView.ViewHolder(listItem) {
@@ -27,7 +32,8 @@ class PartnerBookingAdapter(private val myList: ArrayList<Booking>) :
         val statusBooking: ImageView = listItem.findViewById(R.id.statusBookingIV)
         val titleBooking: TextView = listItem.findViewById(R.id.titleBooking)
         val checkInDate: TextView = listItem.findViewById(R.id.checkInDateBooking)
-        val numberOfNight: TextView = listItem.findViewById(R.id.bookingDate)
+        val bookingDate: TextView = listItem.findViewById(R.id.bookingDate)
+        val numberOfNight: TextView = listItem.findViewById(R.id.numOfNightBooking)
         val roomInfo: TextView = listItem.findViewById(R.id.roomInfoBooking)
         val totalPrice: TextView = listItem.findViewById(R.id.totalPriceBooking)
     }
@@ -42,21 +48,34 @@ class PartnerBookingAdapter(private val myList: ArrayList<Booking>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val bookingOrder = myList[position]
 
-        holder.propertyName.text = bookingOrder.propertyName
-        holder.titleBooking.text = bookingOrder.title
-        holder.checkInDate.text = bookingOrder.checkInDate
-        holder.totalPrice.text = bookingOrder.totalPrice
-        holder.numberOfNight.text = bookingOrder.numberOfNight.toString() + " day(s) ago"
-        holder.roomInfo.text = bookingOrder.roomInfo
+        holder.propertyName.text = bookingOrder.hotel?.hotel_name
 
-        if (bookingOrder.status) {
-            holder.statusBooking.setImageResource(R.drawable.calendar_check_svgrepo_com)
-            holder.statusBooking.setBackgroundResource(R.drawable.bg_icon_green)
+        when (bookingOrder.status) {
+            "Active" -> {
+                holder.titleBooking.text = "${bookingOrder.personal_contact?.name} made a reservation"
+                holder.statusBooking.setImageResource(R.drawable.calendar_check_svgrepo_com)
+                holder.statusBooking.setBackgroundResource(R.drawable.bg_icon_green)
+            }
+            "Completed" -> {
+                holder.titleBooking.text = "${bookingOrder.personal_contact?.name}'reservation has been accepted"
+                holder.statusBooking.setImageResource(R.drawable.calendar_check_svgrepo_com)
+                holder.statusBooking.setBackgroundResource(R.drawable.bg_icon_green)
+            }
+            else -> {
+                holder.titleBooking.text = "${bookingOrder.personal_contact?.name}'reservation has been cancelled"
+                holder.statusBooking.setImageResource(R.drawable.calendar_cross_svgrepo_com)
+                holder.statusBooking.setBackgroundResource(R.drawable.bg_icon_red)
+            }
         }
-        else {
-            holder.statusBooking.setImageResource(R.drawable.calendar_cross_svgrepo_com)
-            holder.statusBooking.setBackgroundResource(R.drawable.bg_icon_red)
-        }
+
+        val format = SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.ENGLISH)
+        holder.checkInDate.text = format.format(bookingOrder.date_start!!.toDate())
+
+        val numberFormat = NumberFormat.getCurrencyInstance(Locale("vn", "VN"))
+        holder.totalPrice.text = numberFormat.format(bookingOrder.total_price)
+        holder.numberOfNight.text = bookingOrder.num_of_nights.toString() + " day(s)"
+        holder.roomInfo.text = bookingOrder.rooms[0].name
+        holder.bookingDate.text = format.format(bookingOrder.created_date!!.toDate())
     }
 
     override fun getItemCount(): Int = myList.size
